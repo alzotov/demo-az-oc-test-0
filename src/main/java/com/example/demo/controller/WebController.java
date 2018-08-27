c0e9e104c74d03bf9e0a353ecc1925cb9bf1d1b3
alzotov <azotov@hotmail.com>
2018-08-27 14:28:36 +0800

    simplified web-method

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -1,20 +1,10 @@
 package com.example.demo.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
-
-/*    @RequestMapping("/hello")
-    public Map<String, String> getCommitId() {
-        Map<String, String> result = new HashMap<>();
-        result.put("msg","Hello, World");
-        return result;
-    }*/
 
     @RequestMapping("/hello")
     public String getCommitId() {
